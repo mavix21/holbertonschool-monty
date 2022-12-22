0ee150f021c4a6dbae9c001fe10a520e57d57479
mavix21 <marcevizcarra@hotmail.com>
2022-12-22 06:49:04 -0800

    Set unused variable line_number in pall function

--- opcodes_functions.c
+++ opcodes_functions.c
@@ -32,6 +32,7 @@ void push(stack_t **stack, unsigned int line_number)
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top;
+	(void)line_number;
 
 	if (*stack == NULL)
 		return;
