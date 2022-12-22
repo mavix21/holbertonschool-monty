45bef1d7aab677e327da820d54dc9f0f0b2b81bb
AlExito745 <alxtinoco1988@gmail.com>
2022-12-22 12:25:51 -0800

    update style betty

--- main.c
+++ main.c
@@ -1,44 +1,48 @@
 #include "monty.h"
 
-char *arg_str = NULL;
+/**
+ * main - driver function for monty program
+ * @argc: int num of arguments
+ * @argv: opcode file
+ * Return: 0
+*/
 
 int main(int argc, char *argv[])
 {
-    char buf[1024] = {0};
-    FILE *fp = NULL;
-    char *opcode = NULL;
-    int line_number;
-    stack_t *stack = NULL;
+	char buf[1024] = {0};
+	FILE *fp = NULL;
+	char *opcode = NULL;
+	int line_number;
+	stack_t *stack = NULL;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fp = fopen(argv[1], "r");
 
-    fp = fopen(argv[1], "r");
-    if (fp == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (fp == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	line_number = 1;
 
-    line_number = 1;
-    while (fgets(buf, sizeof(buf), fp) != NULL)
-    {
-        opcode = strtok(buf, " \n");
-        if (opcode == NULL)
-        {
-            line_number++;
-            continue;
-        }
-        arg_str = strtok(NULL, " \n");
-        exec_opcode(opcode, &stack, line_number);
-        line_number++;
-    }
+	while (fgets(buf, sizeof(buf), fp) != NULL)
+	{
+		opcode = strtok(buf, " \n");
 
-    fclose(fp);
-    free_stack(&stack);
-
-    return (EXIT_SUCCESS);
+		if (opcode == NULL)
+		{
+			line_number++;
+			continue;
+		}
+		arg_str = strtok(NULL, " \n");
+		exec_opcode(opcode, &stack, line_number);
+		line_number++;
+	}
+	fclose(fp);
+	free_stack(&stack);
+	return (EXIT_SUCCESS);
 }
