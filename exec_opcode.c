6fe9d97b924e672d2266097f0fd5850b7f3d4309
mavix21 <marcevizcarra2109@hotmail.com>
2022-12-21 23:16:53 -0500

    Add exec_opcode function

---
+++ exec_opcode.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+void exec_opcode(char *opcode, stack_t **stack, unsigned int line_number)
+{
+    instruction_t opcodes[] = {
+        {"push", push},
+        {"pall", push},
+        {"pint", push},
+        {"pop", push},
+        {"swap", push},
+        {"add", push},
+        {"sub", push},
+        {"nop", push},
+        {NULL, NULL}
+    };
+    int i;
+
+    for (i = 0; opcodes[i].opcode != NULL; i++)
+    {
+        if (!strcmp(opcode, opcodes[i].opcode))
+        {
+            opcodes[i].f(stack, line_number);
+        }
+    }
+}
