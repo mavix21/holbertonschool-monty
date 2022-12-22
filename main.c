2620ed1fa7b8637ec39c7db4889aa7aaa8932d38
mavix21 <marcevizcarra2109@hotmail.com>
2022-12-21 22:55:56 -0500

    Add main function

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+char *arg_str = NULL;
+
+int main(int argc, char *argv[])
+{
+    char buf[1024] = {0};
+    FILE *fp = NULL;
+    char *opcode = NULL;
+    int line_number;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fp = fopen(argv[1], "r");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    line_number = 1;
+    while (fgets(buf, sizeof(buf), fp) != NULL)
+    {
+        opcode = strtok(buf, " \n");
+        if (opcode == NULL)
+        {
+            line_number++;
+            continue;
+        }
+        arg_str = strtok(NULL, " \n");
+        exec_opcode(opcode, line_number);
+        line_number++;
+    }
+
+    return (EXIT_SUCCESS);
+}
