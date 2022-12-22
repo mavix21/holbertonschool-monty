785f664d8e55fe198f0efa0893e517682682854c
mavix21 <marcevizcarra2109@hotmail.com>
2022-12-22 00:00:01 -0500

    Add is_numeric function

--- helpers.c
+++ helpers.c
@@ -20,3 +20,23 @@ void free_stack(stack_t **stack)
 	free_stack_recursive(*stack);
 	*stack = NULL;
 }
+
+int is_numeric(char *s)
+{
+	if (s == NULL || strlen(s) == 0)
+		return (0);
+
+	if (*s == '-' && *(s + 1) != '\0')
+		s++;
+
+	while (*s != '\0')
+	{
+		if (*s < '0' || *s > '9')
+		{
+			return (0);
+		}
+		s++;
+	}
+
+	return (1);
+}
