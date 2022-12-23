966af63acc9597735279d7782c7b3e6a57f0fa9b
mavix21 <marcevizcarra2109@hotmail.com>
2022-12-23 14:38:36 -0500

    Add mul_stack function

--- opcodes_functions2.c
+++ opcodes_functions2.c
@@ -78,7 +78,7 @@ void nop(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * div - divides the second top element of the stack by
+ * div_stack - divides the second top element of the stack by
  * the top element of the stack
  * @stack: pointer to linked list stack
  * @line_number: number of line opcode occurs on
@@ -104,6 +104,40 @@ void div_stack(stack_t **stack, unsigned int line_number)
 		panic("division by zero", line_number);
 
 	new_node->n = (top->next)->n / top->n;
+	new_node->prev = NULL;
+	new_node->next = (top->next)->next;
+	if ((top->next)->next != NULL)
+		((top->next)->next)->prev = new_node;
+
+	(top->next)->next = NULL;
+	*stack = new_node;
+	free_stack(&top);
+}
+
+/**
+ * mul_stack - multiplies the second top element of the stack by
+ * the top element of the stack
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+void mul_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node;
+	stack_t *top;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		panic("can't mul, stack too short", line_number);
+
+	new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	top = *stack;
+	new_node->n = (top->next)->n * top->n;
 	new_node->prev = NULL;
 	new_node->next = (top->next)->next;
 	if ((top->next)->next != NULL)
