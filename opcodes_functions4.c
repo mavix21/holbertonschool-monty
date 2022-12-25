23d51a765639621514fd02308fdc69133898e20d
mavix21 <marcevizcarra2109@hotmail.com>
2022-12-24 20:57:53 -0500

    Implement stack and queue opcodes

---
+++ opcodes_functions4.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * stack_f - sets the format of the data to a stack (LIFO)
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+void stack_f(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+
+	info.data_format = LIFO;
+}
+
+/**
+ * queue_f - sets the format of the data to a queue (FIFO)
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+void queue_f(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+
+	info.data_format = FIFO;
+}
