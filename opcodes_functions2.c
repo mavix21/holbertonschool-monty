eb36cf0469eaadf2423012acc6ce259156940eca
mavix21 <marcevizcarra2109@hotmail.com>
2022-12-24 19:56:31 -0500

    Adapt code to work with panic and panic_malloc

--- opcodes_functions2.c
+++ opcodes_functions2.c
@@ -12,14 +12,14 @@ void add(stack_t **stack, unsigned int line_number)
 	stack_t *top;
 
 	if (*stack == NULL || (*stack)->next == NULL)
-		panic("can't add, stack too short", line_number);
+	{
+		free_stack(stack);
+		panic("can't add, stack too short", stack, line_number);
+	}
 
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+		panic_malloc(stack);
 
 	top = *stack;
 	new_node->n = top->n + (top->next)->n;
@@ -44,15 +44,12 @@ void sub(stack_t **stack, unsigned int line_number)
 	stack_t *top;
 
 	if (*stack == NULL || (*stack)->next == NULL)
-		panic("can't sub, stack too short", line_number);
+		panic("can't sub, stack too short", stack, line_number);
 
 	new_node = malloc(sizeof(stack_t));
 
 	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+		panic_malloc(stack);
 
 	top = *stack;
 	new_node->n = (top->next)->n - top->n;
@@ -89,19 +86,16 @@ void div_stack(stack_t **stack, unsigned int line_number)
 	stack_t *top;
 
 	if (*stack == NULL || (*stack)->next == NULL)
-		panic("can't div, stack too short", line_number);
+		panic("can't div, stack too short", stack, line_number);
 
 	new_node = malloc(sizeof(stack_t));
 
 	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+		panic_malloc(stack);
 
 	top = *stack;
 	if (top->n == 0)
-		panic("division by zero", line_number);
+		panic("division by zero", stack, line_number);
 
 	new_node->n = (top->next)->n / top->n;
 	new_node->prev = NULL;
@@ -126,15 +120,12 @@ void mul_stack(stack_t **stack, unsigned int line_number)
 	stack_t *top;
 
 	if (*stack == NULL || (*stack)->next == NULL)
-		panic("can't mul, stack too short", line_number);
+		panic("can't mul, stack too short", stack, line_number);
 
 	new_node = malloc(sizeof(stack_t));
 
 	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+		panic_malloc(stack);
 
 	top = *stack;
 	new_node->n = (top->next)->n * top->n;
